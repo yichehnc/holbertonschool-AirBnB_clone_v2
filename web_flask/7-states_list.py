797a27efa2b0d0ec7b89e323bbfac15b705c739a
yichehnc <yichenc2017@gmail.com>
2024-04-15 00:19:59 +1000

    feat: script that uses storage for fetching data from the storage engine

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+A script that starts a Flask web application
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """
+    Closes the storage
+    """
+    storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """
+    Displays an HTML page with a list of all states
+    """
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda state: state.name)
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
